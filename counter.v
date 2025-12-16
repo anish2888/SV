5bea9bcf22ca8df1f398387ea3086cba77e7aab5
anish2888 <sharma.anish28@gmail.com>
2025-12-16 19:10:40 +0530

    Update counter.v

--- counter.v
+++ counter.v
@@ -1,25 +1,12 @@
-`timescale 1ns/1ps
-module counter_tb; 
-reg clk,rst,m;
-wire [3:0] count;
-counter d (count,rst,clk,m);
-initial
-begin
-clk=0;
-rst=0;
-#25;
-rst=1;
-end
-initial
-begin
-m=1;
-#600 m=0;
-rst=0;
-#25;
-rst=1;
-#500 m=0;
-end
-always #5 clk=~clk;
-initial
-#1400 $finish;
-endmodule
+add_ndr -width {Metal1 0.12 Metal2 0.14 Metal3 0.14 Metal4 0.14 Metal5 0.14 Metal6 0.14 Metal7 0.14 Metal8 0.14 Metal9 0.14 } -spacing {Metal1 0.12 Metal2 0.14 Metal3 0.14 Metal4 0.14 Metal5 0.14 Metal6 0.14 Metal7 0.14 Metal8 0.14 Metal9 0.14 } -name 2w2s
+create_route_type -name clkroute -non_default_rule 2w2s -bottom_preferred_layer Metal5 -top_preferred_layer Metal6
+
+.......
+set_ccopt_property route_type clkroute -net_type trunk
+set_ccopt_property route_type clkroute -net_type leaf
+set_ccopt_property buffer_cells {CLKBUFX8 CLKBUFX12}
+set_ccopt_property inverter_cells {CLKINVX8 CLKINVX12}
+set_ccopt_property clock_gating_cells TLATNTSCA*
+create_ccopt_clock_tree_spec -file ccopt.spec
+source ccopt.spec
+clock_opt_design -cts
